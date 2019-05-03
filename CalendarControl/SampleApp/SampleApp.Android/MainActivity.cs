1b95e184238c90de03c6cd9c2b9dfd0ce9d35c97
Josip Caleta <jcaleta1337@gmail.com>
2019-05-03 20:39:10 +0200

    Remove Xamarin.Essentials from sample project

--- CalendarControl/SampleApp/SampleApp.Android/MainActivity.cs
+++ CalendarControl/SampleApp/SampleApp.Android/MainActivity.cs
@@ -1,6 +1,5 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
 
 namespace SampleApp.Droid
@@ -15,15 +14,8 @@ namespace SampleApp.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
