0e53797364cdfc8df231556a66bf2d9aae285f8b
Josip Ćaleta <josip.caleta@lilcodelab.com>
2019-05-10 19:34:04 +0200

    Add BindableBase abstract class for bindable property handling with dependency support

---
+++ src/Calendar.Plugin/Shared/Models/BindableBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Xamarin.Plugin.Calendar.Models
+{
+    internal abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Dictionary<string, PropertyChangedEventArgs> _propertyChangedArgs = new Dictionary<string, PropertyChangedEventArgs>();
+        private readonly Dictionary<string, object> _properties = new Dictionary<string, object>();
+        private readonly Dictionary<string, List<PropertyChangedEventArgs>> _propertyDependencies = new Dictionary<string, List<PropertyChangedEventArgs>>();
+
+        protected TData GetProperty<TData>(TData defaultValue = default, [CallerMemberName] string propertyName = "")
+        {
+            if (!_properties.ContainsKey(propertyName))
+                AddProperty(propertyName, defaultValue);
+
+            return (TData)_properties[propertyName];
+        }
+
+        protected void SetProperty<TData>(TData value, [CallerMemberName] string propertyName = "")
+        {
+            if (!_properties.ContainsKey(propertyName))
+                AddProperty(propertyName, value);
+            else if (value.Equals(_properties[propertyName]))
+                return;
+
+            _properties[propertyName] = value;
+            PropertyChanged?.Invoke(this, _propertyChangedArgs[propertyName]);
+
+            if (_propertyDependencies.TryGetValue(propertyName, out List<PropertyChangedEventArgs> alsoNotifyFor))
+            {
+                foreach (var dependentPropertyArgs in alsoNotifyFor)
+                    PropertyChanged?.Invoke(this, dependentPropertyArgs);
+            }
+        }
+
+        protected void SetupPropertyDependencies(string propertyName, params string[] dependentProperties)
+        {
+            var dependentPropertyArgs = new List<PropertyChangedEventArgs>();
+
+            foreach (var dependentProperty in dependentProperties)
+            {
+                if (!_propertyChangedArgs.ContainsKey(dependentProperty))
+                    _propertyChangedArgs.Add(dependentProperty, new PropertyChangedEventArgs(dependentProperty));
+
+                dependentPropertyArgs.Add(_propertyChangedArgs[dependentProperty]);
+            }
+
+            _propertyDependencies.Add(propertyName, dependentPropertyArgs);
+        }
+
+        private void AddProperty(string propertyName, object defaultValue)
+        {
+            if (!_propertyChangedArgs.ContainsKey(propertyName))
+                _propertyChangedArgs.Add(propertyName, new PropertyChangedEventArgs(propertyName));
+
+            _properties.Add(propertyName, defaultValue);
+        }
+    }
+}
--- src/Calendar.Plugin/Shared/Models/DayModel.cs
+++ src/Calendar.Plugin/Shared/Models/DayModel.cs
@@ -1,102 +1,87 @@
 ﻿using System;
-using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace Xamarin.Plugin.Calendar.Models
 {
-    internal class DayModel : INotifyPropertyChanged
+    internal class DayModel : BindableBase
     {
-        private static readonly PropertyChangedEventArgs _datePropertyArgs = new PropertyChangedEventArgs(nameof(Date));
-        private static readonly PropertyChangedEventArgs _hasEventsPropertyArgs = new PropertyChangedEventArgs(nameof(HasEvents));
-        private static readonly PropertyChangedEventArgs _isThisMonthPropertyArgs = new PropertyChangedEventArgs(nameof(IsThisMonth));
-        private static readonly PropertyChangedEventArgs _isSelectedPropertyArgs = new PropertyChangedEventArgs(nameof(IsSelected));
-        private static readonly PropertyChangedEventArgs _eventColorPropertyArgs = new PropertyChangedEventArgs(nameof(EventColor));
-        private static readonly PropertyChangedEventArgs _selectedTextColorColorPropertyArgs = new PropertyChangedEventArgs(nameof(SelectedTextColor));
-        private static readonly PropertyChangedEventArgs _otherMonthColorPropertyArgs = new PropertyChangedEventArgs(nameof(OtherMonthColor));
-        private static readonly PropertyChangedEventArgs _deselectedTextColorPropertyArgs = new PropertyChangedEventArgs(nameof(DeselectedTextColor));
-        private static readonly PropertyChangedEventArgs _selectedBackgroundColorPropertyArgs = new PropertyChangedEventArgs(nameof(SelectedBackgroundColor));
-        private static readonly PropertyChangedEventArgs _deselectedBackgroundColorPropertyArgs = new PropertyChangedEventArgs(nameof(DeselectedBackgroundColor));
-        private static readonly PropertyChangedEventArgs _textColorPropertyArgs = new PropertyChangedEventArgs(nameof(TextColor));
-        private static readonly PropertyChangedEventArgs _backgroundColorPropertyArgs = new PropertyChangedEventArgs(nameof(BackgroundColor));
-
-        private DateTime _date;
-        private bool _hasEvents;
-        private bool _isThisMonth;
-        private bool _isSelected;
-        private Color _selectedTextColor = Color.White;
-        private Color _otherMonthColor = Color.Silver;
-        private Color _deselectedTextColor = Color.Default;
-        private Color _selectedBackgroundColor = Color.FromHex("#2196F3");
-        private Color _deselectedBackgroundColor = Color.Transparent;
-        private Color _eventIndicatorColor = Color.FromHex("#FF4081");
-        private Color _eventIndicatorSelectedColor = Color.FromHex("#FF4081");
-
-        public event PropertyChangedEventHandler PropertyChanged;
+        public DayModel()
+        {
+            SetupPropertyDependencies(nameof(IsThisMonth), nameof(TextColor));
+            SetupPropertyDependencies(nameof(IsSelected), nameof(TextColor), nameof(BackgroundColor), nameof(EventColor));
+            SetupPropertyDependencies(nameof(SelectedTextColor), nameof(TextColor));
+            SetupPropertyDependencies(nameof(OtherMonthColor), nameof(TextColor));
+            SetupPropertyDependencies(nameof(DeselectedTextColor), nameof(TextColor));
+            SetupPropertyDependencies(nameof(SelectedBackgroundColor), nameof(BackgroundColor));
+            SetupPropertyDependencies(nameof(DeselectedBackgroundColor), nameof(BackgroundColor));
+            SetupPropertyDependencies(nameof(EventIndicatorColor), nameof(EventColor));
+            SetupPropertyDependencies(nameof(EventIndicatorSelectedColor), nameof(EventColor));
+        }
 
         public DateTime Date
         {
-            get => _date;
-            set => SetProperty(ref _date, value, _datePropertyArgs);
+            get => GetProperty<DateTime>();
+            set => SetProperty(value);
         }
 
         public bool HasEvents
         {
-            get => _hasEvents;
-            set => SetProperty(ref _hasEvents, value, _hasEventsPropertyArgs);
+            get => GetProperty<bool>();
+            set => SetProperty(value);
         }
 
         public bool IsThisMonth
         {
-            get => _isThisMonth;
-            set => SetProperty(ref _isThisMonth, value, _isThisMonthPropertyArgs, _textColorPropertyArgs);
+            get => GetProperty<bool>();
+            set => SetProperty(value);
         }
-        
+
         public bool IsSelected
         {
-            get => _isSelected;
-            set => SetProperty(ref _isSelected, value, _isSelectedPropertyArgs, _textColorPropertyArgs, _backgroundColorPropertyArgs, _eventColorPropertyArgs);
+            get => GetProperty<bool>();
+            set => SetProperty(value);
         }
 
         public Color SelectedTextColor
         {
-            get => _selectedTextColor;
-            set => SetProperty(ref _selectedTextColor, value, _selectedTextColorColorPropertyArgs, _textColorPropertyArgs);
+            get => GetProperty(Color.White);
+            set => SetProperty(value);
         }
 
         public Color OtherMonthColor
         {
-            get => _otherMonthColor;
-            set => SetProperty(ref _otherMonthColor, value, _otherMonthColorPropertyArgs, _textColorPropertyArgs);
+            get => GetProperty(Color.Silver);
+            set => SetProperty(value);
         }
 
         public Color DeselectedTextColor
         {
-            get => _deselectedTextColor;
-            set => SetProperty(ref _deselectedTextColor, value, _deselectedTextColorPropertyArgs, _textColorPropertyArgs);
+            get => GetProperty(Color.Default);
+            set => SetProperty(value);
         }
 
         public Color SelectedBackgroundColor
         {
-            get => _selectedBackgroundColor;
-            set => SetProperty(ref _selectedBackgroundColor, value, _selectedBackgroundColorPropertyArgs, _backgroundColorPropertyArgs);
+            get => GetProperty(Color.FromHex("#2196F3"));
+            set => SetProperty(value);
         }
 
         public Color DeselectedBackgroundColor
         {
-            get => _deselectedBackgroundColor;
-            set => SetProperty(ref _deselectedBackgroundColor, value, _deselectedBackgroundColorPropertyArgs, _backgroundColorPropertyArgs);
+            get => GetProperty(Color.Transparent);
+            set => SetProperty(value);
         }
 
         public Color EventIndicatorColor
         {
-            get => _eventIndicatorColor;
-            set => SetProperty(ref _eventIndicatorColor, value, _eventColorPropertyArgs);
+            get => GetProperty(Color.FromHex("#FF4081"));
+            set => SetProperty(value);
         }
 
         public Color EventIndicatorSelectedColor
         {
-            get => _eventIndicatorSelectedColor;
-            set => SetProperty(ref _eventIndicatorSelectedColor, value, _eventColorPropertyArgs);
+            get => GetProperty(Color.FromHex("#FF4081"));
+            set => SetProperty(value);
         }
 
         public Color EventColor => IsSelected
@@ -110,23 +95,5 @@ namespace Xamarin.Plugin.Calendar.Models
         public Color TextColor => IsSelected
                                 ? SelectedTextColor
                                 : IsThisMonth ? DeselectedTextColor : OtherMonthColor;
-
-        private void SetProperty<TData>(
-            ref TData storage,
-            TData value,
-            PropertyChangedEventArgs eventArgs,
-            params PropertyChangedEventArgs[] alsoNotifyFor
-            ) where TData : struct
-        {
-            if (storage.Equals(value))
-                return;
-
-            storage = value;
-
-            PropertyChanged?.Invoke(this, eventArgs);
-
-            foreach (var item in alsoNotifyFor)
-                PropertyChanged?.Invoke(this, item);
-        }
     }
 }
