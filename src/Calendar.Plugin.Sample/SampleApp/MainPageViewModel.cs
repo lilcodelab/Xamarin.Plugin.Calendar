0a43f00ecdf65c81cbfb818dd205c8ce20228df4
Josip Caleta <jcaleta1337@gmail.com>
2019-05-21 18:17:20 +0200

    Change sample to be more readable and add ObservableCollection sample

--- src/Calendar.Plugin.Sample/SampleApp/MainPageViewModel.cs
+++ src/Calendar.Plugin.Sample/SampleApp/MainPageViewModel.cs
@@ -5,6 +5,8 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace SampleApp
 {
@@ -19,78 +21,37 @@ namespace SampleApp
             // testing all kinds of adding events
             Events = new EventCollection
             {
-                [DateTime.Now.AddDays(-3)] = new List<EventModel>
-                {
-                    new EventModel { Name = "Cool event1", Description = "This is Cool event1's description!" },
-                    new EventModel { Name = "Cool event2", Description = "This is Cool event2's description!" },
-                    new EventModel { Name = "Cool event3", Description = "This is Cool event3's description!" },
-                    new EventModel { Name = "Cool event4", Description = "This is Cool event4's description!" },
-                    new EventModel { Name = "Cool event5", Description = "This is Cool event5's description!" },
-                }
+                [DateTime.Now.AddDays(-3)] = new List<EventModel>(GenerateEvents(10, "Cool")),
             };
 
-            Events.Add(DateTime.Now.AddDays(-2), new List<EventModel>
-            {
-                new EventModel { Name = "Cool event1", Description = "This is Cool event1's description!" },
-                new EventModel { Name = "Cool event2", Description = "This is Cool event2's description!" },
-                new EventModel { Name = "Cool event3", Description = "This is Cool event3's description!" },
-                new EventModel { Name = "Cool event4", Description = "This is Cool event4's description!" },
-                new EventModel { Name = "Cool event5", Description = "This is Cool event5's description!" },
-            });
-
-            Events[DateTime.Now] = new List<EventModel>
-            {
-                new EventModel { Name = "Boring event1", Description = "This is Boring event1's description!" },
-                new EventModel { Name = "Boring event2", Description = "This is Boring event2's description!" },
-            };
+            Events.Add(DateTime.Now.AddDays(-1), new List<EventModel>(GenerateEvents(5, "Cool")));
+            
+            Events[DateTime.Now] = new List<EventModel>(GenerateEvents(2, "Boring"));
 
             Task.Delay(5000).ContinueWith(_ =>
             {
-                Events[DateTime.Now] = new List<EventModel>
-                {
-                    new EventModel { Name = "Boring event1", Description = "This is Boring event1's description!" },
-                    new EventModel { Name = "Boring event2", Description = "This is Boring event2's description!" },
-                    new EventModel { Name = "Boring event3", Description = "This is Boring event3's description!" },
-                    new EventModel { Name = "Boring event4", Description = "This is Boring event4's description!" },
-                    new EventModel { Name = "Boring event5", Description = "This is Boring event5's description!" },
-                    new EventModel { Name = "Boring event6", Description = "This is Boring event6's description!" },
-                    new EventModel { Name = "Boring event7", Description = "This is Boring event7's description!" },
-                    new EventModel { Name = "Boring event8", Description = "This is Boring event8's description!" },
-                    new EventModel { Name = "Boring event9", Description = "This is Boring event9's description!" },
-                    new EventModel { Name = "Boring event10", Description = "This is Boring event10's description!" },
-                };
+                Events[DateTime.Now] = new ObservableCollection<EventModel>(GenerateEvents(10, "Cool"));
+                Events.Add(DateTime.Now.AddDays(3), new List<EventModel>(GenerateEvents(5, "Cool")));
 
-                Events.Add(DateTime.Now.AddDays(3), new List<EventModel>
-                {
-                    new EventModel { Name = "Cool event6", Description = "This is Cool event6's description!" },
-                    new EventModel { Name = "Cool event7", Description = "This is Cool event7's description!" },
-                    new EventModel { Name = "Cool event8", Description = "This is Cool event8's description!" },
-                    new EventModel { Name = "Cool event9", Description = "This is Cool event9's description!" },
-                    new EventModel { Name = "Cool event10", Description = "This is Cool event10's description!" },
-                });
+                Events[DateTime.Now.AddDays(10)] = new List<EventModel>(GenerateEvents(10, "Boring"));
+                Events.Add(DateTime.Now.AddDays(15), new List<EventModel>(GenerateEvents(10, "Cool")));
 
-                Events[DateTime.Now.AddDays(10)] = new List<EventModel>
+                Task.Delay(3000).ContinueWith(t =>
                 {
-                    new EventModel { Name = "Boring event11", Description = "This is Boring event11's description!" },
-                    new EventModel { Name = "Boring event12", Description = "This is Boring event12's description!" },
-                    new EventModel { Name = "Boring event13", Description = "This is Boring event13's description!" },
-                    new EventModel { Name = "Boring event14", Description = "This is Boring event14's description!" },
-                    new EventModel { Name = "Boring event15", Description = "This is Boring event15's description!" },
-                    new EventModel { Name = "Boring event16", Description = "This is Boring event16's description!" },
-                    new EventModel { Name = "Boring event17", Description = "This is Boring event17's description!" },
-                    new EventModel { Name = "Boring event18", Description = "This is Boring event18's description!" },
-                    new EventModel { Name = "Boring event19", Description = "This is Boring event19's description!" },
-                };
-
-                Events.Add(DateTime.Now.AddDays(15), new List<EventModel>
-                {
-                    new EventModel { Name = "Cool event11", Description = "This is Cool event11's description!" },
-                    new EventModel { Name = "Cool event12", Description = "This is Cool event12's description!" },
-                    new EventModel { Name = "Cool event13", Description = "This is Cool event13's description!" },
-                    new EventModel { Name = "Cool event14", Description = "This is Cool event14's description!" },
-                    new EventModel { Name = "Cool event15", Description = "This is Cool event15's description!" },
+                    var todayEvents = Events[DateTime.Now] as ObservableCollection<EventModel>;
+                    todayEvents.Insert(0, new EventModel { Name = "Cool event insert", Description = "This is Cool event's description!" });
+                    todayEvents.Add(new EventModel { Name = "Cool event add", Description = "This is Cool event's description!" });
                 });
             }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private IEnumerable<EventModel> GenerateEvents(int count, string name)
+        {
+            return Enumerable.Range(1, count).Select(x => new EventModel
+            {
+                Name = $"{name} event{x}",
+                Description = $"This is {name} event{x}'s description!"
+            });
         }
 
         public EventCollection Events { get; }
