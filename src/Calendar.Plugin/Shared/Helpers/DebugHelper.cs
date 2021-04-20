7868b581791e09b07205707ed87204c287be5476
MatijaLuketin <matija.luketin@lilcodelab.com>
2021-04-20 10:29:11 +0200

    Added DebugHelper class to make measuring performance easier

---
+++ src/Calendar.Plugin/Shared/Helpers/DebugHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Xamarin.Plugin.Calendar.Helpers
+{
+    public static class DebugHelper
+    {
+        public static void MeasureMethodExecutionTime(Action action)
+        {
+    #if DEBUG
+            var sv = Stopwatch.StartNew();
+
+            action.Invoke();
+
+            sv.Stop();
+            Debug.WriteLine($"{action.Method.Name} execution time: {sv.Elapsed}");
+    #endif
+        }
+    }
+}
